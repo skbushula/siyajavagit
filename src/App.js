e97f2e9148afec78cfa679b96402215b6475b758
Siyabonga <skbushula@gmail.com>
2022-05-14 14:50:53 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+//import Header from './components/ui/Header'
+
+const App = () => {
+    return ( <
+        div className = "Container" >
+        Hello Siya <
+        /div>
+    );
+}
+
+export default App;
---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../../img/logo.png'
+const header = () => {
+    return ( <
+        Header className = 'centered' >
+        <
+        img src = { logo }
+        alt = '' < /header>
+    )
+}
+
+export default header
+import logo from '../../img/logo.png'
